ccad06bcbb8eac30f13ecc193f6169e9adaff8c0
Daniel Spira <daniel.b.spira@gmail.com>
2019-09-11 22:43:54 -0700

    inital app functionality working as intended

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendsData = [
+    {
+        name: "Keanu Reeves",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Keanu_Reeves_%28crop_and_levels%29_%28cropped%29.jpg/440px-Keanu_Reeves_%28crop_and_levels%29_%28cropped%29.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Nicolas Cage",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Nicolas_Cage_Deauville_2013.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "John Cena",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/John_Cena_July_2018.jpg/440px-John_Cena_July_2018.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+
+module.exports = friendsData;
